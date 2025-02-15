1f219522f8e11c78857c8b93e3ad3374fdda96e7
jordan <jrobey72@gmail.com>
2025-02-15 17:17:53 -0500

    adding back git and tmux

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,75 +1,80 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--branch=stable",
+		lazyrepo,
+		lazypath,
+	})
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Basic Settings
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.g.python3_host_prog = vim.fn.system("pyenv which python"):gsub("%s*$", "")
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.o.background = "dark"
 
+-- Editor Options
+vim.opt.number = true
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 250
+vim.opt.hlsearch = true
+vim.opt.timeoutlen = 300
+vim.opt.tabstop = 4 -- Number of spaces tabs count for
+vim.opt.softtabstop = 4 -- Number of spaces for a tab while editing
+vim.opt.shiftwidth = 4 -- Size of an indent
+vim.opt.expandtab = true
+-- Basic Keymaps
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Plugin Specifications
 require("lazy").setup({
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	-- Syntax and Language Support
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 	},
+
+	-- Theme and UI
+	{ "Mofiqul/vscode.nvim" },
 	{
-		"williamboman/mason.nvim"
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 	},
 	{
-		"neovim/nvim-lspconfig"
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-	},
-	{ "hrsh7th/nvim-cmp"},
-	{"hrsh7th/cmp-nvim-lsp"},
-	{
-		"L3MON4D3/LuaSnip",
-		-- follow latest release.
-		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-		-- install jsregexp (optional!).
-		build = "make install_jsregexp"
-	},
-	{
-		'stevearc/overseer.nvim',
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
 		opts = {},
-	},
-	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
-	{
-		'windwp/nvim-autopairs',
-		event = "InsertEnter",
-		config = true
-		-- use opts = {} for passing setup options
-		-- this is equalent to setup({}) function
-	},
-	{
-		'numToStr/Comment.nvim',
-		opts = {
-			-- add any options here
-		},
-		lazy = false,
-	},
-	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		-- or                              , branch = '0.1.x',
-		dependencies = { 'nvim-lua/plenary.nvim' }
-	},
-	{
-		'nvim-lualine/lualine.nvim',
-		dependencies = { 'nvim-tree/nvim-web-devicons' }
 	},
 	{
 		"j-hui/fidget.nvim",
-		opts = {
-			-- options
-		},
+		opts = {},
+	},
+
+	-- Editor Enhancement
+	{
+		"L3MON4D3/LuaSnip",
+		version = "v2.*",
+		build = "make install_jsregexp",
 	},
 	{
 		"folke/which-key.nvim",
@@ -78,177 +83,243 @@ require("lazy").setup({
 			vim.o.timeout = true
 			vim.o.timeoutlen = 300
 		end,
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		}
+		opts = {},
+	},
+	"tpope/vim-sleuth",
+
+	-- Code Manipulation
+	{
+		"numToStr/Comment.nvim",
+		opts = {},
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
+	"windwp/nvim-ts-autotag",
+
+	-- Project Management
+	{
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+	},
+	"nvim-lua/plenary.nvim",
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+	},
+
+	-- LSP and Completion
+	{
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+		"hrsh7th/nvim-cmp",
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	"mason-org/mason-registry",
+
+	-- Formatting
+	{
+		"stevearc/conform.nvim",
+		opts = {},
 	},
 })
 
-vim.cmd.colorscheme "catppuccin-mocha"
+-- Theme Configuration
+require("vscode").setup({
+	color_overrides = {
+		vscBack = "#000000",
+	},
+})
+vim.cmd.colorscheme("vscode")
 
-require('overseer').setup()
-vim.api.nvim_set_keymap('n', '<leader>tr', ':OverseerRun<CR>', {})
-vim.api.nvim_set_keymap('n', '<leader>tt', ':OverseerToggle<CR>', {})
-
-
-require("ibl").setup()
-require('lualine').setup()
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python"},
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
-
-  -- List of parsers to ignore installing (or "all")
-  -- ignore_install = { "javascript" },
-
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-  highlight = {
-    enable = true,
-
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    disable = { "c", "rust" },
-    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-    disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-            return true
-        end
-    end,
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-}
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
--- note: diagnostics are not exclusive to lsp servers
--- so these can be global keybindings
-vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
-vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
-vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  desc = 'LSP actions',
-  callback = function(event)
-    local opts = {buffer = event.buf}
-
-    -- these will be buffer-local keybindings
-    -- because they only work if you have an active language server
-
-    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-  end
+-- Plugin Configurations
+-- Autotag
+require("nvim-ts-autotag").setup({
+	opts = {
+		enable_close = true,
+		enable_rename = true,
+		enable_close_on_slash = false,
+	},
+	per_filetype = {
+		["html"] = {
+			enable_close = false,
+		},
+	},
 })
 
-local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+-- Indent Lines
+require("ibl").setup()
+
+-- Status Line
+require("lualine").setup({
+	sections = {
+		lualine_x = { "overseer" },
+	},
+})
+
+-- Treesitter
+require("nvim-treesitter.configs").setup({
+	ensure_installed = {
+		"bash",
+		"html",
+		"scss",
+		"css",
+		"lua",
+		"luadoc",
+		"markdown",
+		"vim",
+		"vimdoc",
+		"javascript",
+		"vue",
+		"python",
+	},
+	auto_install = true,
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = { "ruby" },
+	},
+	indent = {
+		enable = true,
+		disable = { "ruby" },
+	},
+})
+
+require("telescope").setup({
+	pickers = {
+		buffers = {
+			mappings = {
+				n = {
+					["d"] = "delete_buffer",
+				},
+			},
+		},
+	},
+})
+-- Telescope Keybindings
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+-- Diagnostic Keymaps
+vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>")
+vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
+vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
+
+-- LSP Configuration
+vim.api.nvim_create_autocmd("LspAttach", {
+	desc = "LSP actions",
+	callback = function(event)
+		local opts = { buffer = event.buf }
+
+		vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+		vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+		vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+		vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+		vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+		vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+		vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+		vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+		vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+		vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+	end,
+})
+
+-- LSP Server Setup
+local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local default_setup = function(server)
-  require('lspconfig')[server].setup({
-    capabilities = lsp_capabilities,
-  })
+	require("lspconfig")[server].setup({
+		capabilities = lsp_capabilities,
+	})
 end
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  ensure_installed = {},
-  handlers = {
-    default_setup,
-  },
+require("mason").setup()
+require("mason-lspconfig").setup({
+	ensure_installed = { "volar" },
 })
 
-local cmp = require('cmp')
+-- LSP Server Handlers
+require("mason-lspconfig").setup_handlers({
+	default_setup,
+	["volar"] = function()
+		require("lspconfig").volar.setup({
+			filetypes = { "typescript", "javascript", "javascriptreact", "vue" },
+			init_options = {
+				vue = {
+					hybridMode = false,
+				},
+			},
+		})
+	end,
+	-- ["ts_ls"] = function()
+	-- 	local mason_packages = vim.fn.stdpath("data") .. "/mason/packages"
+	-- 	local volar_path = mason_packages .. "/vue-language-server/node_modules/@vue/language-server"
+	-- 	require("lspconfig").ts_ls.setup({
+	-- 		init_options = {
+	-- 			plugins = {
+	-- 				{
+	-- 					name = "@vue/typescript-plugin",
+	-- 					location = volar_path,
+	-- 					languages = { "vue" },
+	-- 				},
+	-- 			},
+	-- 		},
+	-- 	})
+	-- end,
+})
 
+-- Completion Setup
+local cmp = require("cmp")
 cmp.setup({
-  sources = {
-    {name = 'nvim_lsp'},
-  },
-  mapping = cmp.mapping.preset.insert({
-    -- Enter key confirms completion item
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
-
-    -- Ctrl + space triggers completion menu
-    ['<C-Space>'] = cmp.mapping.complete(),
-  }),
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
+	sources = {
+		{ name = "nvim_lsp" },
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
+		["<C-Space>"] = cmp.mapping.complete(),
+	}),
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
 })
 
--- Set highlight on search
-vim.o.hlsearch = false
-
--- Make line numbers default
-vim.wo.number = true
-
--- Enable mouse mode
-vim.o.mouse = 'a'
-
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
-
--- Automatically update buffer if changes from outside nivm
-vim.opt.autoread = true
-vim.api.nvim_create_autocmd({"FocusGained", "BufEnter"}, {
-    pattern = "*",
-    command = "checktime"
+-- Formatting Setup
+require("conform").setup({
+	formatters_by_ft = {
+		python = { "isort", "black" },
+		javascript = { "prettier", stop_after_first = true },
+		typescript = { "prettier" },
+		vue = { "prettier" },
+		lua = { "stylua" },
+	},
+	format_on_save = {
+		timeout_ms = 2500,
+		lsp_fallback = true,
+	},
 })
 
--- Enable break indent
-vim.o.breakindent = true
-vim.o.autoindent = true
+-- Format Commands
+local function format_current_buffer()
+	require("conform").format({
+		bufnr = vim.api.nvim_get_current_buf(),
+	})
+end
 
--- Save undo history
-vim.o.undofile = true
+vim.keymap.set("n", "<leader>cf", format_current_buffer, { noremap = true, silent = true })
 
--- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
-
--- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
-
--- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
-
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-
-
+-- Format on Save
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
+})
